87d6ce600c3a117d2da850cdd32ce498d656f04c
annvlkva <anservol@gmail.com>
2021-05-19 12:16:22 +0300

    drawing table added

--- editorapplication/static/editorapplication/js_files/table.js
+++ editorapplication/static/editorapplication/js_files/table.js
@@ -1,15 +1,15 @@
 const host = '127.0.0.1'//'192.168.0.8'
-let myTable = document.getElementsByClassName("table table-striped")
+    //let myTable = document.getElementsByClassName("table table-striped")
 
 async function onPageLoaded(){
-    let content = document.getElementById("content")
+    //let content = document.getElementById("content")
 
 
-    content.innerHTML = "Loading... <img src=\"https://smallenvelop.com/wp-content/uploads/2014/08/Preloader_11.gif\">"
+    //content.innerHTML = "Loading... <img src=\"https://smallenvelop.com/wp-content/uploads/2014/08/Preloader_11.gif\">"
     try {
         let response = await fetch('http://' + host.toString() + ':8000/get_data')
         let body = await response.json()
-        content.innerHTML = ''
+        //content.innerHTML = ''
 
         let tree = body['tree'].flat()
 
@@ -21,21 +21,10 @@ async function onPageLoaded(){
         console.log(parsedNodes)
         console.log(parsedRelationShips)
 
-        mkTbl('50px', 5)
+        let data = tableData(parsedNodes, parsedRelationShips)
+        console.log("data:   ", data)
 
-
-
-        /*var $table = $('#table')
-
-        $(function(){
-            var tableData = dataToTable(parsedNodes, parsedRelationShips)
-            $table.bootstrapTable({data: tableData})
-        })*/
-
-
-
-
-
+        drawTable(data)
 
         //console.log(tableData)
 
@@ -48,25 +37,84 @@ async function onPageLoaded(){
 
 onPageLoaded()
 
-function dataToTable(nodes, edges){
-    let response = []
-    for (let opk of nodes){
+
+function drawTable(data) {
+    webix.ready(function () {
+        grida = webix.ui({
+            container: "testA",
+            id: "table",
+            view: "treetable",
+            columns: [
+                //{ id:"id",	header:"", css:{"text-align":"right"},  	width:50},
+                {
+                    id: "label", header: "Label", width: 350,
+                    template: "{common.space()}{common.icon()} #label#",
+                    fillspace:1
+                },
+                {id: "node_type", header: "Type", width: 250}
+            ],
+            select: "row",
+            autoheight: true,
+            autowidth: true,
+
+
+            on: {
+                onSelectChange: function () {
+                    console.log()
+                    var text = $$("table").getSelectedId(true).join();
+                    alert("Selected: " + text)
+                }
+            },
+
+            data: data
+        });
+    });
+}
+
+function tableData(nodes, edges){
+    let data;
+    data = [];
+
+    for(let opk of nodes){
         if(opk["node_type"] === "OPK"){
-            col1 = opk["label"]
-            col2 = []
-            for (let des of nodes){
-                if (des["node_type"] === "Descriptor"){
-                    for (let edge of edges){
-                        if(edge["from"] === des["id"] && edge["to"] === opk["id"]){
-                            col2.push(des["label"])
+            let ind_data;
+            ind_data = [];
+            for(let ind of nodes){
+                if(ind["node_type"] === "Indicator"){
+                    for(let ind_edge of edges){
+                        if(ind_edge["from"] === ind["id"] && ind_edge["to"] === opk["id"]){
+                            let des_data;
+                            des_data = [];
+                            for(let des of nodes){
+                                if(des["node_type"] === "Descriptor"){
+                                    for (let des_edge of edges){
+                                        if(des_edge["from"] === des["id"] && des_edge["to"] === ind["id"]){
+                                            des_data.push({
+                                                "id": des["id"],
+                                                "label": des["label"],
+                                                "node_type": des["node_type"]
+                                            })
+                                        }
+                                    }
+                                }
+                            }
+                            ind_data.push({
+                                "id": ind["id"],
+                                "label": ind["label"],
+                                "node_type": ind["node_type"],
+                                "data": des_data
+                            })
                         }
                     }
                 }
             }
-            response.push({"OPK": col1, "Descriptors": col2})
+            data.push({
+                "id": opk["id"],
+                "label": opk["label"],
+                "node_type": opk["node_type"],
+                "data": ind_data
+            })
         }
     }
-
-    return response
-
+    return data
 }
